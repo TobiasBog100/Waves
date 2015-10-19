151343e8fa30adf6ba5d7571c07448d899455075
TobiasBog100 <tobias.bogenfeld@t-online.de>
2015-10-19 11:41:11 +0200

    Update Umwandler_001.py

--- Umwandler_001.py
+++ Umwandler_001.py
@@ -1,3 +1,7 @@
 from cx_Freeze import setup, Executable
 
 setup( name = "window" , version = "0.1" , description = "exe" , executables = [Executable("window.py")] , )
+
+# python file
+# using by freeze
+# window.py in window.exe
